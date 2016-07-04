8d05f32198522ae51e5c6a3a2f66ee58b2ab5077
codemattcode <codemattcode@gmail.com>
2016-07-04 09:08:35 +0100

    added lightbox

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 'use strict';
 
-$(document).ready(function(){
+var groups = {};
+$('.galleryItem').each(function() {
+  var id = parseInt($(this).attr('data-group'), 10);
+  
+  if(!groups[id]) {
+    groups[id] = [];
+  } 
+  
+  groups[id].push( this );
+});
 
-  // code
 
+$.each(groups, function() {
+  
+  $(this).magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: false,
+      gallery: { enabled:true }
+  })
+  
 });
 
 
